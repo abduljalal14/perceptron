2e7687af5b23580083a56b34ee638cd08e668a6c
Abdul Jalal <73058070+abduljalal14@users.noreply.github.com>
2025-06-02 16:33:28 +0700

    pisah objek ke file

--- myperceptron.js
+++ myperceptron.js
@@ -1,37 +1,34 @@
-// Perceptron Object
+// Perceptron Object ---------------------
 function Perceptron(no, learningRate = 0.00001) {
+  // Set Initial Values
+  this.learnc = learningRate;
+  this.bias = 1;
 
-// Set Initial Values
-this.learnc = learningRate;
-this.bias = 1;
-console.log("Perceptron di buat dengan " + no + " input dan " + this.learnc + " learning rate");
+  // Compute Random Weights
+  this.weights = [];
+  for (let i = 0; i <= no; i++) {
+    this.weights[i] = Math.random() * 2 - 1;
+  }
 
-// Compute Random Weights
-console.log("Menentukan bobot awal secara acak antara -1 hingga 1");
-this.weights = [];
-for (let i = 0; i <= no; i++) {
-  this.weights[i] = 0;
+  // Activate Function
+  this.activate = function(inputs) {
+    let sum = 0;
+    for (let i = 0; i < inputs.length; i++) {
+      sum += inputs[i] * this.weights[i];
+    } 
+    if (sum > 0) {return 1} else {return 0}
+  }
+
+  // Train Function
+  this.train = function(inputs, desired) {
+    inputs.push(this.bias);
+    let guess = this.activate(inputs);
+    let error = desired - guess;
+    if (error != 0) {
+      for (let i = 0; i < inputs.length; i++) {
+        this.weights[i] += this.learnc * error * inputs[i];         
+      }
+    }
+  }
 }
-console.log("Bobot awal: " + this.weights[0]);
-
-// Activate Function
-this.activate = function(inputs) {
-  let sum = 0;
-  console.log(inputs + " * " + this.weights[0] + " + sum =" + "= " + sum);
-  let tebakan = sum > 0 ? 1 : 0;
-  console.log("Output Perceptron data ini adalah: " + tebakan);
-  let posisi = sum > 0 ? "diatas" : "dibawah";
-  console.log("Artinya titiknya " + posisi + " garis pemisah");
-  if (sum > 0) {return 1} else {return 0}
-}
-
-// Train Function
-this.train = function(inputs, desired) {
-  inputs.push(this.bias);
-  let guess = this.activate(inputs);
-  let error = desired - guess;
-  
-}
-
 // End Perceptron Object
-}
